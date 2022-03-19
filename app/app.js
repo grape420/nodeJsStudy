1729c219fafa3ded75d3935192fbd0e1c24edff4
grape420 <grape420@naver.com>
2022-03-19 19:59:14 +0900

    20220319 19:58 프론트단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -25,5 +25,6 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home);    // use -> 미들 웨어를 등록해주는 메소드.
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
