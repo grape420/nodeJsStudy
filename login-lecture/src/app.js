b12b9720f96793555308efed2756632fa8b19ba0
grape420 <grape420@naver.com>
2022-03-19 18:40:00 +0900

    20220319 18:39 불필요한 파일 정리

--- login-lecture/src/app.js
+++ login-lecture/src/app.js
@@ -17,8 +17,6 @@
 const express = require("express");
 const app = express();
 
-const POST = 3000;
-
 // 라우팅
 const home = require("./routes/home");
 
@@ -28,7 +26,4 @@ app.set("view engine", "ejs");
 
 app.use("/", home);    // use -> 미들 웨어를 등록해주는 메소드.
 
-
-app.listen(POST, () => {
-  console.log("서버 가동");
-});
+module.exports = app;
